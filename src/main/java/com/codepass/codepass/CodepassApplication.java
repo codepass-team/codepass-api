90222433ddc7caf1e85b0ed5bb080ac255d802d5
Frezcirno <2277861660@qq.com>
2020-12-10 20:36:14 +0800

    Init commit

---
+++ src/main/java/com/codepass/codepass/CodepassApplication.java
@@ -0,0 +1,13 @@
+package com.codepass.codepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodepassApplication.class, args);
+    }
+
+}
