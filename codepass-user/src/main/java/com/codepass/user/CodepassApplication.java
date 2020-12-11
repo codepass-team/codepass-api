01f4b994e33678c8c8e1d75a455df40819bc34c5
Frezcirno <2277861660@qq.com>
2020-12-11 09:26:30 +0800

    项目模块化

--- codepass-user/src/main/java/com/codepass/user/CodepassApplication.java
+++ codepass-user/src/main/java/com/codepass/user/CodepassApplication.java
@@ -1,4 +1,4 @@
-package com.codepass.codepass;
+package com.codepass.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- codepass-user/src/test/java/com/codepass/user/CodepassApplicationTests.java
+++ codepass-user/src/test/java/com/codepass/user/CodepassApplicationTests.java
@@ -1,4 +1,4 @@
-package com.codepass.codepass;
+package com.codepass.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
