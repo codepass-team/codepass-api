9522ae07b32833a1db2c87ff8b1ab851361d04ff
Frezcirno <2277861660@qq.com>
2020-12-20 18:17:00 +0800

    refresh object after save

--- codepass-user/src/main/java/com/codepass/user/CodepassApplication.java
+++ codepass-user/src/main/java/com/codepass/user/CodepassApplication.java
@@ -1,15 +1,12 @@
 package com.codepass.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.codepass.user.dao.CustomRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-
-import javax.sql.DataSource;
-import java.util.Properties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomRepositoryImpl.class)
 public class CodepassApplication {
 
     public static void main(String[] args) {
