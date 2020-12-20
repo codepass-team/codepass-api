18b28ec74e1057c05894f232f29ac2ab1de82311
CaSiOFT <liuwenshuo2000@126.com>
2020-12-20 21:15:57 +0800

    关注问题

---
+++ codepass-user/src/main/java/com/codepass/user/dao/FollowQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.codepass.user.dao;
+
+import com.codepass.user.dao.entity.FollowQuestionEntity;
+import com.codepass.user.dao.entity.FollowQuestionEntityPK;
+
+public interface FollowQuestionRepository extends CustomRepository<FollowQuestionEntity, FollowQuestionEntityPK> {
+}
