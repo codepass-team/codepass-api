4448e116b37652358ced8d78de37e94e7aa4ae8e
Frezcirno <2277861660@qq.com>
2020-12-21 23:03:51 +0800

    增加docker跳转接口

--- codepass-user/src/main/java/com/codepass/user/service/DockerService.java
+++ codepass-user/src/main/java/com/codepass/user/service/DockerService.java
@@ -1,5 +1,8 @@
 package com.codepass.user.service;
 
+import com.codepass.user.dao.DockerRepository;
+import com.codepass.user.dao.entity.DockerEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +17,8 @@ import java.util.UUID;
 public class DockerService {
     @Value("${docker.storage.path}")
     private String dockerStoragePath;
+    @Autowired
+    DockerRepository dockerRepository;
 
     public String createDocker() throws IOException {
         String dockerId = UUID.randomUUID().toString().replace("-", "").substring(0, 8);
@@ -21,6 +26,11 @@ public class DockerService {
                 Path.of(dockerStoragePath + dockerId),
                 PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwxrwxrwx"))
         );
+        DockerEntity dockerEntity = new DockerEntity();
+        dockerEntity.setId(dockerId);
+        dockerEntity.setPort(0);
+        dockerEntity.setStatus(0);
+        dockerRepository.save(dockerEntity);
         return dockerId;
     }
 
@@ -30,19 +40,30 @@ public class DockerService {
         return newDockerId;
     }
 
-    public int mountDocker(String dockerId, String password) throws IOException {
+    public DockerEntity mountDocker(String dockerId, String password) throws IOException {
+        DockerEntity dockerEntity = dockerRepository.findById(dockerId).get();
         String filePath = dockerStoragePath + dockerId;
-        String mountPath = "/home/codepass";
+        String mountPath = "/home/coder/project";
         int port = new Random().nextInt(10000) + 10000;
-        Process process = Runtime.getRuntime().exec("timeout 1800 " +
-                "docker run --env PASSWORD=" + password + " -it " +
-                "-p 0.0.0.0:" + port + ":8080 " +
-                "-v " + filePath + ":" + mountPath + " " +
-                "codercom/code-server");
-        return port;
+        Process process = Runtime.getRuntime().exec("timeout 1800" +
+                " docker run --rm -it" +
+                " --name " + dockerId +
+                " --env PASSWORD=" + password +
+                " -p 0.0.0.0:" + port + ":8080" +
+                " -v " + filePath + ":" + mountPath +
+                " codercom/code-server");
+        dockerEntity.setPassword(password);
+        dockerEntity.setStatus(1);
+        dockerRepository.save(dockerEntity);
+        return dockerEntity;
     }
 
     public boolean umountDocker(String dockerId) {
         return true;
+    }
+
+    public String getUri(String dockerId, String baseUri) {
+        DockerEntity dockerEntity = dockerRepository.findById(dockerId).get();
+        return baseUri.split(":/")[0] + ":" + dockerEntity.getPort() + "/?folder=/home/coder/project";
     }
 }
