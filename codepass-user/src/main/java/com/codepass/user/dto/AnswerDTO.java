9928c2273ca47775f0d578991295ea974d04b1e6
1851788 <1654475161@qq.com>
2020-12-22 17:33:12 +0800

    Update AnswerDTO.java

--- codepass-user/src/main/java/com/codepass/user/dto/AnswerDTO.java
+++ codepass-user/src/main/java/com/codepass/user/dto/AnswerDTO.java
@@ -2,9 +2,11 @@ package com.codepass.user.dto;
 
 import com.codepass.user.dao.entity.AnswerEntity;
 import com.codepass.user.dao.entity.QuestionEntity;
+import com.codepass.user.dao.entity.UserEntity;
 
 public class AnswerDTO extends AnswerEntity{
     private String questionContent;
+    private UserEntity questioner;
 
     public AnswerDTO(AnswerEntity a,QuestionEntity q){
         this.setId(a.getId());
@@ -16,6 +18,7 @@ public class AnswerDTO extends AnswerEntity{
         this.setStatus(a.getStatus());
         this.setQuestionId(a.getQuestionId());
         this.setQuestionContent(q.getContent());
+        this.setQuestioner(q.getQuestioner());
     }
 
     public String getQuestionContent() {
@@ -24,5 +27,13 @@ public class AnswerDTO extends AnswerEntity{
 
     public void setQuestionContent(String questionContent) {
         this.questionContent = questionContent;
+    }
+
+    public UserEntity getQuestioner() {
+        return questioner;
+    }
+
+    public void setQuestioner(UserEntity questioner) {
+        this.questioner = questioner;
     }
 }
